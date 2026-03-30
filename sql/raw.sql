d98f7167c0c7f00104a8da524ad5e4f403d830e7
pranjalverma-cs22 <122616536+pranjalverma-cs22@users.noreply.github.com>
2026-03-30 12:00:01 +0530

    Update raw.sql

--- sql/raw.sql
+++ sql/raw.sql
@@ -1,1 +1,13 @@
+USE DATABASE Gittrial;
+Use SCHEMA TRIAL;
 
+
+CREATE OR REPLACE TABLE RAW_TABLE (
+    id INT,
+    name STRING
+);
+
+INSERT INTO RAW_TABLE (id, name) VALUES
+(1, 'pranjal'),
+(2, 'snowflake'),
+(3, 'pipeline');
