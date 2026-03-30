a6ddb0c8538b0d59678ed5a7e0cf161f1ecc197b
pranjalverma-cs22 <122616536+pranjalverma-cs22@users.noreply.github.com>
2026-03-30 12:50:58 +0530

    Update data_quality.sql

--- tests/data_quality.sql
+++ tests/data_quality.sql
@@ -1,10 +1,11 @@
-USE DATABASE Gittrial;
+USE DATABASE GITTRIAL;
 USE SCHEMA TRIAL;
  
+-- Fail pipeline if NULL IDs exist
 SELECT
     CASE
-        WHEN COUNT(*) > 0 THEN
-            ERROR('Null IDs found!')
+        WHEN COUNT(*) > 0 THEN 1/0   -- forces error
+        ELSE 1
     END
 FROM CLEAN_TABLE
 WHERE id IS NULL;
